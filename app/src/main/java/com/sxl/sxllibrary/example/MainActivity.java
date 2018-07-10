72da6d697247f332fb5073283562a4cdff5a66c0
MonkeySunxl <1185962238@qq.com>
2018-07-10 15:05:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/sxl/sxllibrary/example/MainActivity.java
+++ app/src/main/java/com/sxl/sxllibrary/example/MainActivity.java
@@ -28,46 +28,46 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         SXLSingle.init(this);
         Context context = SXLSingle.getContext();
-        Log.i("aaa", "onCreate: "+context);
-        Log.i("aaa", "onCreate: "+this);
-        HomeLoader homeLoader = new HomeLoader();
-        homeLoader.getVersonInfo().subscribe(new SxlSubscriber<BaseInfoBean<VersionBean>>() {
-            @Override
-            public void onSuccess(BaseInfoBean<VersionBean> infoBean) {
-                Log.i("检测版本更新1", "onNext: "+infoBean.getInfo().toString());
-            }
+//         Log.i("aaa", "onCreate: "+context);
+//         Log.i("aaa", "onCreate: "+this);
+//         HomeLoader homeLoader = new HomeLoader();
+//         homeLoader.getVersonInfo().subscribe(new SxlSubscriber<BaseInfoBean<VersionBean>>() {
+//             @Override
+//             public void onSuccess(BaseInfoBean<VersionBean> infoBean) {
+//                 Log.i("检测版本更新1", "onNext: "+infoBean.getInfo().toString());
+//             }
 
-            @Override
-            protected void onFinish() {
+//             @Override
+//             protected void onFinish() {
 
-            }
-        });
-        BaseUrl.API_BASE_URL="http://api6.pkusky.org/index.php/index/";
-        SXLSingle.getBuilder().baseUrl(BaseUrl.API_BASE_URL);
-        // 添加公共参数拦截器
-        //TODO:
-        SXLSingle.getHttpClientNoCacheBuilder().addInterceptor(new HttpCommonInterceptor());
-        SXLSingle.createService(ApiService.class,false).getVersion1()
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<BaseInfoBean<VersionBean>>() {
-                    @Override
-                    public void onCompleted() {
+//             }
+//         });
+//         BaseUrl.API_BASE_URL="";
+//         SXLSingle.getBuilder().baseUrl(BaseUrl.API_BASE_URL);
+//         // 添加公共参数拦截器
+//         //TODO:
+//         SXLSingle.getHttpClientNoCacheBuilder().addInterceptor(new HttpCommonInterceptor());
+//         SXLSingle.createService(ApiService.class,false).getVersion1()
+//                 .subscribeOn(Schedulers.newThread())
+//                 .observeOn(AndroidSchedulers.mainThread())
+//                 .subscribe(new Subscriber<BaseInfoBean<VersionBean>>() {
+//                     @Override
+//                     public void onCompleted() {
 
-                    }
+//                     }
 
-                    @Override
-                    public void onError(Throwable e) {
+//                     @Override
+//                     public void onError(Throwable e) {
 
-                    }
+//                     }
 
-                    @Override
-                    public void onNext(BaseInfoBean<VersionBean> infoBean) {
-                        Log.i("检测版本更新2", "onNext: "+infoBean.getInfo().toString());
-                        if (infoBean.getStatus() == 1) {
+//                     @Override
+//                     public void onNext(BaseInfoBean<VersionBean> infoBean) {
+//                         Log.i("检测版本更新2", "onNext: "+infoBean.getInfo().toString());
+//                         if (infoBean.getStatus() == 1) {
                             
-                        }
-                    }
-                });
+//                         }
+//                     }
+//                 });
     }
 }
